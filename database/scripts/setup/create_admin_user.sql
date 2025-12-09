46117212dcdda925df5679f4362112e69541e340
Mugara250 <mbonyumugara52@gmail.com>
2025-12-09 18:32:59 +0200

    refactor

--- database/scripts/setup/create_admin_user.sql
+++ database/scripts/setup/create_admin_user.sql
@@ -5,26 +5,26 @@
 -- ============================================
 
 -- Ensure we're in the correct PDB
-ALTER SESSION SET CONTAINER = mon_[StudentID]_[FirstName]_VendorScoring_DB;
+ALTER SESSION SET CONTAINER = mon_27906_mushi_vendorperformanceanalytics_db;
 
 PROMPT Creating project tablespaces...
 
 -- 1. DATA tablespace for user tables
 CREATE TABLESPACE vendor_data 
-DATAFILE '/u01/app/oracle/oradata/CDB1/mon_[StudentID]_[FirstName]_VendorScoring_DB/vendor_data01.dbf'
+DATAFILE 'C:\oracle\oradata\ORCL\mon_27906_mushi_vendorperformanceanalytics_db\VENDOR_DATA01.DBF'
 SIZE 200M AUTOEXTEND ON NEXT 100M MAXSIZE 2G
 EXTENT MANAGEMENT LOCAL 
 SEGMENT SPACE MANAGEMENT AUTO;
 
 -- 2. INDEX tablespace for indexes
 CREATE TABLESPACE vendor_idx 
-DATAFILE '/u01/app/oracle/oradata/CDB1/mon_[StudentID]_[FirstName]_VendorScoring_DB/vendor_idx01.dbf'
+DATAFILE 'C:\oracle\oradata\ORCL\mon_27906_mushi_vendorperformanceanalytics_db\VENDOR_IDX01.DBF'
 SIZE 100M AUTOEXTEND ON NEXT 50M MAXSIZE 1G
 EXTENT MANAGEMENT LOCAL;
 
 -- 3. TEMPORARY tablespace for sorting operations
 CREATE TEMPORARY TABLESPACE vendor_temp
-TEMPFILE '/u01/app/oracle/oradata/CDB1/mon_[StudentID]_[FirstName]_VendorScoring_DB/vendor_temp01.dbf'
+TEMPFILE 'C:\oracle\oradata\ORCL\mon_27906_mushi_vendorperformanceanalytics_db\VENDOR_TEMP01.DBF'
 SIZE 100M AUTOEXTEND ON NEXT 25M MAXSIZE 500M;
 
 -- Verify tablespace creation
